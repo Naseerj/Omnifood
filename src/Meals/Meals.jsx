8d4da4806bb1ff693bbea481c541d1286f76707f
Naseerj <jenrolaolasupo@gmail.com>
2023-09-04 03:44:08 +0100

    Fixing bugs

--- src/Meals/Meals.jsx
+++ src/Meals/Meals.jsx
@@ -13,22 +13,25 @@ const Meals = () => {
   const [filteredMeals, setFilteredMeals] = useState([]);
   const [dataGotten, setDataGotten] = useState("");
   const [btn, setBtn] = useState("");
-
-  // const { api, dataGotten } = Data();
-  // const {apiCategory, category} = DataCategory()
-
-  // const { category, apiCategory } = DataCategory();
-  // console.log(apiCategory);
+  const [cats, setCats] = useState('')
+  const hideApi = useRef();
+ 
 
   const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-  let cat = "Vegetarian";
+  let cat = cats;
+
+  const changeCategory = () =>{
+    
+  }
+
+
 
   const filterCategory = () => {
-    if (btn === "Side") {
-      console.log("Working");
-      cat = " Beef";
-    }
+    hideApi.current.style.display = "none";
+    setCats('Vegetarian')
+    // var cat = cat[1]
+    
   };
 
   const fetchData = async () => {
@@ -51,80 +54,88 @@ const Meals = () => {
   }, []);
 
   return (
-    <div className="allmeal">
-      <p>MEALS</p>
-      <h2>Omnifood AI chooses from 5,000+ recipes</h2>
-      <div className="renderbtn">
-        <button className="all">All</button>
-        <button
-          className="side"
-          onClick={() => {
-            setBtn("Side");
-            filterCategory();
-            // changeSide()
-          }}
-        >
-          Side
-        </button>
-        <button className="beef">Beef</button>
-        <button className="vegetarian">Vegetarian</button>
-      </div>
+    <div>
+      <div className="allmeal">
+        <p>MEALS</p>
+        <h2>Omnifood AI chooses from 5,000+ recipes</h2>
+        <div className="renderbtn">
+          <button className="all">All</button>
+          <button
+            className="side"
+            onClick={() => {
+              setBtn("Side");
+              filterCategory();
+              changeCategory()
+              console.log(cat)
+              // changeSide()
+            }}
+          >
+            Side
+          </button>
+          <button onClick={(()=>{
+            setCats(['Beef'])
+          })} className="beef">Beef</button>
+          <button onClick={(()=>{
+            setCats(['Vegetarian'])
+          })} className="vegetarian">Vegetarian</button>
+        </div>
 
-      <div className={"loading-container " + (dataGotten ? "hidden" : "")}>
-        <div className="loading"></div>
-        <div id="loading-text">loading</div>
-      </div>
+        <div className={"loading-container " + (dataGotten ? "hidden" : "")}>
+          <div className="loading"></div>
+          <div id="loading-text">loading</div>
+        </div>
 
-      <div className={"api " + (dataGotten ? "" : "hidden")}>
-        {dataGotten
-          ? api.slice(1, 10).map((datum) => {
-              const { strCategory, strMealThumb, idMeal, strMeal } = datum;
-              return (
-                <div className="parentgrid" key={idMeal}>
-                  <div className="">
-                    <div className="gridrender">
-                      <div className="showcase1render">
-                        <img
-                          className="mealb"
-                          src={strMealThumb}
-                          alt={strMeal}
-                        />
-                        <div className="tagsdiv1">
-                          <button>{strCategory}</button>
-                          <h2>{strMeal}</h2>
-                          <p>
-                            {" "}
-                            <span>
-                              <IoFlameOutline />{" "}
-                            </span>
-                            {Math.floor(Math.random() * 100)} calories
-                          </p>
-                          <p>
-                            {" "}
-                            <span>
-                              <IoRestaurantOutline />{" "}
-                            </span>
-                            Nutriscore
-                          </p>
-                          <p>
-                            {" "}
-                            <span>
-                              <IoStarOutline />{" "}
-                            </span>
-                            {Math.floor(Math.random() * 5)} rating ® 5
-                          </p>
+        <div ref={hideApi} className={"api " + (dataGotten ? "" : "hidden")}>
+          {dataGotten
+            ? api.slice(1, 10).map((datum) => {
+                const { strCategory, strMealThumb, idMeal, strMeal } = datum;
+                return (
+                  <div className="parentgrid" key={idMeal}>
+                    <div className="">
+                      <div className="gridrender">
+                        <div className="showcase1render">
+                          <img
+                            className="mealb"
+                            src={strMealThumb}
+                            alt={strMeal}
+                          />
+                          <div className="tagsdiv1">
+                            <button>{strCategory}</button>
+                            <h2>{strMeal}</h2>
+                            <p>
+                              {" "}
+                              <span>
+                                <IoFlameOutline />{" "}
+                              </span>
+                              {Math.floor(Math.random() * 100)} calories
+                            </p>
+                            <p>
+                              {" "}
+                              <span>
+                                <IoRestaurantOutline />{" "}
+                              </span>
+                              Nutriscore
+                            </p>
+                            <p>
+                              {" "}
+                              <span>
+                                <IoStarOutline />{" "}
+                              </span>
+                              {Math.floor(Math.random() * 5)} rating ® 5
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })
-          : // <div class="loading-container">
-            //   <div class="loading"></div>
-            //   <div id="loading-text">loading</div>
-            // </div>
-            "yoo"}
+                );
+              })
+            : // <div class="loading-container">
+              //   <div class="loading"></div>
+              //   <div id="loading-text">loading</div>
+              // </div>
+              "yoo"}
+        </div>
       </div>
       <div className="categorisedDiv">
         {filteredMeals.map((category) => {
